95bb7a48fc17495e6af8375bb63e9d144cefbc01
Rika-L <2209393334@qq.com>
2024-09-22 23:46:37 +0800

    修改dev命令

--- demo/src/app.controller.ts
+++ demo/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('rika')
+  getRika(): string {
+    return this.appService.getRika();
+  }
 }
